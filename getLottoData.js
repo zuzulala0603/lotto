1a9faf526c6bd8d082a4998523b1999622caf746
zuzulala0603 <zuzulala0603@gmail.com>
2020-07-28 23:44:27 +0900

    finish

--- getLottoData.js
+++ getLottoData.js
@@ -1,21 +1,11 @@
-const axios = require("axios")
-const fs = require("fs")
-const { resolve } = require("path")
-const { rejects } = require("assert")
+const axios = require("axios");
+const fs = require("fs");
+const { resolve } = require("path");
+const { rejects } = require("assert");
 
-let drwNo = 1
-let result = []
-let result2 = []
-
-/* async function saveLottoData() {
-  for (var i = 1; i < 920; i++) {
-    await getData(i).then((data) => {
-      result.push(data)
-    })
-  }
-  console.log(result)
-  fs.writeFileSync("sample.txt", JSON.stringify(result), "utf8")
-} */
+let drwNo = 1;
+let result = [];
+let result2 = [];
 
 async function saveLottoData() {
   for (var i = 1; i < 920; i++) {
@@ -30,53 +20,23 @@ async function saveLottoData() {
         data.bnusNo,
         data.drwNo,
         data.firstWinamnt,
-      ]
-    })
+      ];
+    });
   }
 
-  fs.writeFileSync("lottoData.txt", JSON.stringify(result), "utf8")
+  fs.writeFileSync("lottoData.txt", JSON.stringify(result), "utf8");
 }
 
 function getData(num) {
   return new Promise(function (resolve, reject) {
     const lottoData = axios.get(`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${num}`).then((data) => {
       if (data.data.drwNo) {
-        resolve(data.data)
+        resolve(data.data);
       } else {
-        reject("error")
+        reject("error");
       }
-    })
-  })
+    });
+  });
 }
 
-saveLottoData()
-/* for (let drwNo = 1; drwNo < 10; drwNo++) {
-  const lottoData = axios
-    .get(`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${drwNo}`)
-    .then((data) => {
-      result.push(data.data)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-    .then(() => console.log(result))
-    .catch((err) => {
-      console.log(err)
-    })
-} */
-/* for (let drwNo = 1; drwNo < 10; drwNo++) {
-  const lottoData = axios
-    .get(`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${drwNo}`)
-    .then((data) => {
-      console.log(data.data.drwNo)
-      result.push(data.data.drwNo)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-console.log(result) */
-/* fs.writeFileSync("big.txt", result, "utf8") */
-/* for (let i = 0; i < 50; i++) {
-  fs.writeFileSync("target.txt", JSON.stringify(data.data), "utf8")
-} */
+saveLottoData();
